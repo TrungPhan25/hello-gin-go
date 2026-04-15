41ada1ae991c3a42d37d8fea0b8b206dbc74bc9d
Phan Thế Trung (Toby) <toby@cybridge.jp>
2026-04-15 15:28:29 +0700

    url basic

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
@@ -9,6 +11,29 @@ func main() {
 			"message": "Hello World",
 		})
 	})
+
+	r.GET("/users", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"users": []string{"Alice", "Bob", "Charlie"},
+		})
+	})
+
+	r.GET("/user/:user_id", func(c *gin.Context) {
+		user_id := c.Param("user_id")
+		name_query := c.Query("name")
+		c.JSON(200, gin.H{
+			"data":    "Thoong tin user",
+			"user_id": user_id,
+			"name":    name_query,
+		})
+	})
+
+	r.GET("/products", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"products": "Danh sach san pham",
+		})
+	})
+
 	r.Run(":8080")
 
 }
